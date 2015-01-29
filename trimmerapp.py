ffc3fd9b8075db4719daffd0cffad70ee217c1c4
Stanislav Ageev <stanislav.ageev@conduit.com>
2015-01-29 14:30:35 +0200

    Update

--- trimmerapp.py
+++ trimmerapp.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response, abort
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.errorhandler(409)
+def not_found(error):
+    return make_response(jsonify({'error': 'Record with such name already exists'}), 409)
+
+@app.route('/v1/service/register', methods=['POST'])
+def register_service():
+    abort(409)
+
+@app.route('/v1/service/<int:service_id>/queue', methods=['GET'])
+def get_service_queue(service_id):
+    abort(404)
+
+@app.route('/v1/service/<int:service_id>/queue/add', methods=['POST'])
+def add_to_queue(service_id):
+    abort(404)
+
+@app.route('/v1/service/<int:service_id>/queue/get', methods=['GET'])
+def get_from_queue(service_id):
+    abort(404)
+
+@app.route('/v1/service/<int:service_id>/queue/remove', methods=['DELETE'])
+def remove_from_queue(service_id):
+    abort(404)
 
 
 if __name__ == '__main__':
