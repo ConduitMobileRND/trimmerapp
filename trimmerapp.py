f4b6cac881baf846f86daaa1b5210e376e77fc2e
Stanislav Ageev <stanislav.ageev@conduit.com>
2015-01-29 15:01:26 +0200

    Register service

--- trimmerapp.py
+++ trimmerapp.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, make_response, abort
+from flask import Flask, jsonify, make_response, abort, request
 
 app = Flask(__name__)
+
+service_map = {}
 
 @app.errorhandler(404)
 def not_found(error):
@@ -10,9 +12,18 @@ def not_found(error):
 def not_found(error):
     return make_response(jsonify({'error': 'Record with such name already exists'}), 409)
 
+
+
 @app.route('/v1/service/register', methods=['POST'])
 def register_service():
-    abort(409)
+    name = request.json.get('name')
+    address = request.json.get('address')
+    if name not in service_map:
+        abort(409)
+    service = {'name': name, 'address': address, 'queue': {}}
+    service_map[name] = service
+    return jsonify(service), 201
+
 
 @app.route('/v1/service/<int:service_id>/queue', methods=['GET'])
 def get_service_queue(service_id):
